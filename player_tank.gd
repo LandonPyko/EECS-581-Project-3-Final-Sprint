ac8cffbf660419e804dcb5aa2053abd38424459f
Aiden Patel <aiden.patel21@gmail.com>
2024-10-23 00:30:38 -0500

    Added documentation and started to work on some sort of bullet movement for shooting

--- player_tank.gd
+++ player_tank.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var speed = 400
+@export var speed          = 400
 @export var rotation_speed = 1.5
 
 var rotation_direction = 0
@@ -13,10 +13,9 @@ func get_input():
 	#MAKE TURRENT TURN TOWARDS MOUSE
 	var target = get_viewport().get_mouse_position()
 	$tankGun.rotation = target.angle_to_point(position)-deg_to_rad(-90)
-
+	
 
 func _physics_process(delta):
-	
 	get_input()
 	rotation += rotation_direction * rotation_speed * delta
 	move_and_slide()
