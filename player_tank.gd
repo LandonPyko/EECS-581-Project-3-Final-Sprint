16d50fcc2e12425cb6fdd63776729c466ffeae4f
JohnDNewman <j087n641@ku.edu>
2024-10-22 20:59:29 -0500

    Added naive turret rotation.
    Added a two lines of code to make turret turn towards mouse. Does not account for if mouse is outside window, or even if it's on the same screen.

--- player_tank.gd
+++ player_tank.gd
@@ -5,11 +5,17 @@ extends CharacterBody2D
 
 var rotation_direction = 0
 
+
 func get_input():
 	rotation_direction = Input.get_axis("left", "right")
 	velocity = transform.x * Input.get_axis("down", "up") * speed
+	
+	#MAKE TURRENT TURN TOWARDS MOUSE
+	var target = get_viewport().get_mouse_position()
+	$tankGun.rotation = target.angle_to_point(position)-deg_to_rad(-90)
 
 func _physics_process(delta):
+	
 	get_input()
 	rotation += rotation_direction * rotation_speed * delta
 	move_and_slide()
