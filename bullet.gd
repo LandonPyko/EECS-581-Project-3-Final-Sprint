7fa76a9cdb57fbe6972e0e5fb9978a95a726d01d
JohnDNewman <j087n641@ku.edu>
2024-10-24 13:28:49 -0500

    Adding functionality;change tankbody texture; change how mouse pos is gotten
    Added functionality for bullets and mines. left click to bullet, right to mine. Bullets move in single direction with collision. Mines don't move. both are bullet scenes with an export var to control behavior. collision shape and texture for both are assigned during creation in player_tank script.
    Changed player tank model to have text on the front because I couldn't tell sometimes, Old texture is marked as tank_body(old) in assets folder.
    Changed how mouse pos is gotten, now from a _input function which can be used for other things, it updates a mouse_pos var which then gets called when needed.

--- bullet.gd
+++ bullet.gd
@@ -2,27 +2,22 @@ extends CharacterBody2D
 
 @export var speed          = 400
 @export var rotation_speed = 1.5
+@export var type = "bullet";
 
 var rotation_direction = 0
 var click_position 	   = Vector2()
 var target_position	   = Vector2()
 
 
-func get_input():
-	rotation_direction = Input.get_axis("left", "right")	
-	#velocity = transform.x * Input.get_axis("down", "up") * speed
-	#MAKE TURRENT TURN TOWARDS MOUSE
-	var target = get_viewport().get_mouse_position()
-	$bulletShot.rotation  = target.angle_to_point(position)-deg_to_rad(180)
+#func get_input():
+	#rotation_direction = Input.get_axis("left", "right")	
+	##velocity = transform.x * Input.get_axis("down", "up") * speed
+	##MAKE TURRENT TURN TOWARDS MOUSE
+	#var target = get_viewport().get_mouse_position()
+	##$bulletShot.rotation  = target.angle_to_point(position)-deg_to_rad(180)
 	
 
 func _physics_process(delta):
-	if Input.is_action_just_pressed("shoot"):
-		click_position = get_global_mouse_position()
-		target_position = (click_position - position).normalized()
-		velocity = target_position * speed
+	if type == "bullet":
+		velocity = Vector2(1, 0).rotated(rotation) * speed
 		move_and_slide()
-		
-	get_input()
-	rotation += rotation_direction * rotation_speed * delta
-	move_and_slide()
--- player_tank.gd
+++ player_tank.gd
@@ -1,21 +1,42 @@
 extends CharacterBody2D
 
 @export var speed          = 400
-@export var rotation_speed = 1.5
+@export var rotation_speed = 3.5
+
+@onready var BULLET = preload("res://bullet.tscn")
 
 var rotation_direction = 0
-
+var mouse_pos = Vector2(0,0) #set mouse pos to some place, don't matter.
 
 func get_input():
 	rotation_direction = Input.get_axis("left", "right")
 	velocity = transform.x * Input.get_axis("down", "up") * speed
 	
 	#MAKE TURRENT TURN TOWARDS MOUSE
-	var target = get_viewport().get_mouse_position()
-	$tankGun.rotation = target.angle_to_point(position)-deg_to_rad(-90)
+	$tankGun.global_rotation = mouse_pos.angle_to_point(position)-deg_to_rad(-90)
 	
 
 func _physics_process(delta):
 	get_input()
 	rotation += rotation_direction * rotation_speed * delta
+	if Input.is_action_just_pressed("shoot"):
+		mouse_pos = get_global_mouse_position()
+		var bul = BULLET.instantiate()
+		get_tree().root.add_child(bul)
+		bul.global_rotation = ($tankGun.global_rotation)-deg_to_rad(-90)
+		bul.global_position = $tankGun/fire_loc.global_position
+	elif Input.is_action_just_pressed("mine"):
+		mouse_pos = get_global_mouse_position()
+		var mine = BULLET.instantiate()
+		get_tree().root.add_child(mine)
+		mine.type = "mine"
+		mine.get_node("texture").texture = preload("res://assets/mine.png")
+		mine.get_node("hitbox").shape = preload("res://mine_collision.tres")
+		mine.global_position = global_position
+		#velocity = target_position * speed
 	move_and_slide()
+	
+func _input(event):
+	if event is InputEventMouseMotion:
+		print("Mouse Motion at: ", event.position)
+		mouse_pos = event.position
