00012d5cbb76b82df1296060cb5c2cd05e261e07
JohnDNewman <j087n641@ku.edu>
2024-10-24 21:14:20 -0500

    Added walls and obstacles, commented, trimmed code
    bullet.gd : Added comments, removed unneeded function, added a lifetime timer and bullet deletion when timer expires
    player_tank.gd : Added comments to all code, added export vars to set max number of mines and bullets to have, added vars to track current number of mines and bullets assigned to player. Added functions to decrease number of mines and bullets (Called when bullet timer expires), removed some redundant mouse_pos code that I hated. added tracking for number of bullets and mines
    project : forgot to mention before, renamed some physics layers for my convenience.
    stage.tscn : changed boundaries from collision rects to world boundaries, Added one on each side of screen. added 1 obstacle named wall, currently on the same physics layer as world boundaries. maybe fine, maybe change later for some collision reason.

--- bullet.gd
+++ bullet.gd
@@ -1,23 +1,29 @@
 extends CharacterBody2D
 
-@export var speed          = 400
-@export var rotation_speed = 1.5
-@export var type = "bullet";
+@export var speed          = 400 #speed of bullet
+@export var rotation_speed = 1.5 #not needed rn ##OPTION could do curving bullets?
+@export var type = "bullet";     #assign type of bullet, assume normal ##TODO get rid of when mine scene is created
+@export var parent = preload("res://playerTank.tscn"); #create a dummy instance for the bullet to pretend with
 
-var rotation_direction = 0
-var click_position 	   = Vector2()
-var target_position	   = Vector2()
+var rotation_direction = 0 #not needed rn ##OPTION could do curving bullets?
+var click_position 	   = Vector2() #not needed rn
+var target_position	   = Vector2() #not needed rn
+var dead := false #Check if timer has run out because can't do it when signalling.
 
-
-#func get_input():
-	#rotation_direction = Input.get_axis("left", "right")	
-	##velocity = transform.x * Input.get_axis("down", "up") * speed
-	##MAKE TURRENT TURN TOWARDS MOUSE
-	#var target = get_viewport().get_mouse_position()
-	##$bulletShot.rotation  = target.angle_to_point(position)-deg_to_rad(180)
-	
 
 func _physics_process(delta):
-	if type == "bullet":
+	if type == "bullet": #if normal bullet, do movement logic at  consistient speed and direction.
+						 ##TODO figure out richochet logic
 		velocity = Vector2(1, 0).rotated(rotation) * speed
 		move_and_slide()
+	if dead: #if life_time is out, free the instance
+		free()
+
+
+func _on_life_time_timeout(): #when life_time timer expires
+	#Clear either a bullet or mine ##TODO make it only bullet when mine scene is made
+	if type == "bullet":
+		parent.dec_bullets()
+	elif type == "mine":
+		parent.dec_mines()
+	dead = true #Tell the bullet it's dead.
--- player_tank.gd
+++ player_tank.gd
@@ -2,41 +2,66 @@ extends CharacterBody2D
 
 @export var speed          = 400
 @export var rotation_speed = 3.5
+@export var max_bullets = 5 #OPTION Could make a changeable value for powerups
+@export var max_mines = 1
+var cur_bullets := 0 #create var for cur mines and bullets
+var cur_mines := 0   #walrus, := is used to make it only be of the type of its assignment
+					 #in this case, an int. Just an optimization and helps prevent mishaps
 
-@onready var BULLET = preload("res://bullet.tscn")
+@onready var BULLET = preload("res://bullet.tscn")#load the idea of a bullet to this var
+												  #so we can make some from this script
 
-var rotation_direction = 0
+var rotation_direction = 0   #Just create it
 var mouse_pos = Vector2(0,0) #set mouse pos to some place, don't matter.
 
+#functions to decrease cur number of mines and bullets
+func dec_mines():
+	cur_mines = cur_mines - 1
+func dec_bullets():
+	cur_bullets = cur_bullets - 1
+
+#func to get input from player
 func get_input():
-	rotation_direction = Input.get_axis("left", "right")
-	velocity = transform.x * Input.get_axis("down", "up") * speed
+	rotation_direction = Input.get_axis("left", "right") #Direction to turn
+	velocity = transform.x * Input.get_axis("down", "up") * speed #velocity to move at
 	
 	#MAKE TURRENT TURN TOWARDS MOUSE
 	$tankGun.global_rotation = mouse_pos.angle_to_point(position)-deg_to_rad(-90)
 	
 
 func _physics_process(delta):
-	get_input()
-	rotation += rotation_direction * rotation_speed * delta
-	if Input.is_action_just_pressed("shoot"):
-		mouse_pos = get_global_mouse_position()
-		var bul = BULLET.instantiate()
-		get_tree().root.add_child(bul)
-		bul.global_rotation = ($tankGun.global_rotation)-deg_to_rad(-90)
-		bul.global_position = $tankGun/fire_loc.global_position
-	elif Input.is_action_just_pressed("mine"):
-		mouse_pos = get_global_mouse_position()
-		var mine = BULLET.instantiate()
-		get_tree().root.add_child(mine)
-		mine.type = "mine"
-		mine.get_node("texture").texture = preload("res://assets/mine.png")
-		mine.get_node("hitbox").shape = preload("res://mine_collision.tres")
-		mine.global_position = global_position
-		#velocity = target_position * speed
-	move_and_slide()
+	##MOVEMENT
+	get_input() # get details of movement
+	rotation += rotation_direction * rotation_speed * delta #rotate tank
+	move_and_slide() #move tank with physics, wooo
 	
-func _input(event):
-	if event is InputEventMouseMotion:
-		print("Mouse Motion at: ", event.position)
-		mouse_pos = event.position
+	##SHOOTING LOGIC
+	if Input.is_action_just_pressed("shoot") && (cur_bullets < 5): #check if want to and can fire
+		cur_bullets = cur_bullets + 1 #increase number of bullets fired
+		#create bullet instance for bullet
+		var bul = BULLET.instantiate()
+		bul.parent = get_parent().get_node("Tank") #set parent to this instance for refrence
+		get_tree().root.add_child(bul) #add to game tree at root #no reason not to for now
+		bul.global_rotation = ($tankGun.global_rotation)-deg_to_rad(-90) #do orientation bullshit because graphics are fucked fuck you andrew jk love you
+		bul.global_position = $tankGun/fire_loc.global_position #move to the fire loc so it pretend to fire from turret
+		#Should change a bit so end of bullet is end of turret but meh, like a 5 minute fix I'll leave to someone else
+
+	elif Input.is_action_just_pressed("mine") && (cur_mines < 1):
+		cur_mines = cur_mines + 1 #increase number of mines placed
+		#create bullet instance for mine
+			#TODO create new scene for mines
+		var mine = BULLET.instantiate()
+		mine.get_node("life_time").wait_time = 5 #change so mine lasts longer than bullet #wont need if new scene
+		mine.parent = get_parent().get_node("Tank") #set parent to this instance for refrence
+		get_tree().root.add_child(mine) #add to game tree at root
+		mine.type = "mine" #say its a mine bullet
+		mine.get_node("texture").texture = preload("res://assets/mine.png") #change texture to mine
+		mine.get_node("hitbox").shape = preload("res://mine_collision.tres") #change collision to mine
+		mine.global_position = global_position #place at center of tank #TODO Change something with the rendering so tank is on top
+
+
+
+func _input(event): #get input event if one happens
+	if event is InputEventMouseMotion: #if it is mouse movement
+		#print("Mouse Motion at: ", event.position) #print debug info
+		mouse_pos = event.position #change mouse_pos to new pos
