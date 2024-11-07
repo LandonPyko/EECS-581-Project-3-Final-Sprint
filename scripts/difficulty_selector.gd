a6498b37b9379dbb3d3e689cf05cf4233441644e
AMcFerrin <andrew.mcferrin1@gmail.com>
2024-11-07 13:19:09 -0600

    Added basic menus

---
+++ scripts/difficulty_selector.gd
@@ -0,0 +1,12 @@
+extends Control
+
+
+
+func _on_easy_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/stage.tscn")
+
+func _on_medium_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/stage.tscn")
+
+func _on_hard_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/stage.tscn")
