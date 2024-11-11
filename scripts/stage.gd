7e1cf9e9b7b62055a9fac514ead9ee0ff75bf607
JohnDNewman <j087n641@ku.edu>
2024-11-10 21:23:56 -0600

    Updated navmap usage
    Made a nav map resource for all scenes to use. Rebakes the nav_map every oppurtunity it gets, which is not ideal, find a better solution
    Otherwise just added nav_map as the nav map for all level scenes

--- scripts/stage.gd
+++ scripts/stage.gd
@@ -11,9 +11,10 @@ func _process(delta: float) -> void:
 	var enemies = get_tree().get_nodes_in_group("Enemy")
 	var player = get_tree().get_nodes_in_group("Player")
 	
+	if not $nav_map.is_baking():
+		$nav_map.bake_navigation_polygon()
+	
 	if enemies.is_empty():	# If no enemies left
 		get_tree().change_scene_to_file("res://scenes/roundwin.tscn")
 	if player.is_empty():
 		get_tree().change_scene_to_file("res://scenes/roundloss.tscn")
-
-	
