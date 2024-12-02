c7e842ea458b46f9f3348a049afd83403502fec9
LandonPyko <pykomar2@gmail.com>
2024-12-02 13:29:57 -0600

    Modified inheritance and added speed up

--- scenes/pickup.gd
+++ scenes/pickup.gd
@@ -3,9 +3,20 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if (body.name == "Tank" or body.name == "enemy"):
+		_apply_effect(body)
+		queue_free()
+
+func _apply_effect(body):
+	body.speed += 200
 	pass
