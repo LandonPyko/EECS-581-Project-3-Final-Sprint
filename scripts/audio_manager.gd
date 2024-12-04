53962e721486ab70d1c77776a183668e55da05a4
JohnDNewman <j087n641@ku.edu>
2024-12-03 21:56:39 -0600

    Fixed audio staying pitched up after round end
    audio_manager.gd: changed pitch logic to be in process and reset when current level is 0. Could be optimized back to play_music if we called it whenever a scene change occurs. Don't care to do that right now though

--- scripts/audio_manager.gd
+++ scripts/audio_manager.gd
@@ -6,8 +6,6 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 func play_music():
-	if Global.current_level > 1:
-		pitch_scale = 0.1 * Global.current_level + 1
 	if not is_playing():
 		play()
 
@@ -16,4 +14,7 @@ func stop_music():
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Global.current_level > 1:
+		pitch_scale = 0.1 * Global.current_level + 1
+	elif Global.current_level == 0:
+		pitch_scale = 1
