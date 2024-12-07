8073baf48eabe7c42396dad14cd46989b71af3e7
JohnDNewman <j087n641@ku.edu>
2024-12-06 20:24:36 -0600

    Updated clearing logic
    Updated the clearing logic after a stage to affect pvp and normal stages after a .5 second delay.

--- scripts/stage.gd
+++ scripts/stage.gd
@@ -1,9 +1,27 @@
 extends Node2D
 
+var timer = Timer.new()
+var start_timer = false
+#okay so make a timer, wait for it to expire, then clear everything and switch to screen
+
+func stage_clear() -> bool:
+	var nodes = get_tree().root.get_children()
+	if !start_timer:
+		start_timer = true
+		timer.start(0.5)
+		return false
+	elif timer.time_left == 0.0:
+		for node in nodes:
+			if node.is_in_group("Bullets") or node.is_in_group("Mines"):
+				node.free()
+		return true
+	return false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	AudioManager.play_music()
+	timer.one_shot = true
+	self.add_child(timer)
 	pass # Replace with function body.
 
 
@@ -11,36 +29,17 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	var enemies = get_tree().get_nodes_in_group("Enemy")
 	var player = get_tree().get_nodes_in_group("Player")
-	#var bullets = get_tree().get_nodes_in_group("Bullets")
-	#var mines = get_tree().get_nodes_in_group("Mines")
 	
-	#just get everyhing that exists right now
-	var nodes = get_tree().root.get_children()
-	
-	
+	print(str(timer.time_left))
 	if not $nav_map.is_baking():
 		$nav_map.bake_navigation_polygon()
 	
 	if enemies.is_empty() and player.is_empty():
-		for node in nodes:
-			if node.is_in_group("Bullets") or node.is_in_group("Mines"):
-				node.free()
-		#bullets.clear()
-		#mines.clear()
-		get_tree().change_scene_to_file("res://scenes/roundloss.tscn")
+		if stage_clear():
+			get_tree().change_scene_to_file("res://scenes/roundloss.tscn")
 	elif enemies.is_empty():	# If no enemies left
-		for node in nodes:
-			if node.is_in_group("Bullets") or node.is_in_group("Mines"):
-				node.free()
-		#for nodes in bullets:
-			#remove_child(nodes)
-		#for nodes in mines:
-			#remove_child(nodes)
-		get_tree().change_scene_to_file("res://scenes/roundwin.tscn")
+		if stage_clear():
+			get_tree().change_scene_to_file("res://scenes/roundwin.tscn")
 	elif player.is_empty():
-		for node in nodes:
-			if node.is_in_group("Bullets") or node.is_in_group("Mines"):
-				node.free()
-		#bullets.clear()
-		#mines.clear()
-		get_tree().change_scene_to_file("res://scenes/roundloss.tscn")
+		if stage_clear():
+			get_tree().change_scene_to_file("res://scenes/roundloss.tscn")
