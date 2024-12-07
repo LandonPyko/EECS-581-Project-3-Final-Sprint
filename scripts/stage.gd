efa1b42b8cd5dae32881545873e29bc64f1663c1
JohnDNewman <j087n641@ku.edu>
2024-12-06 19:47:48 -0600

    Fixed bullets and mines persisting
    Added code to get all root nodes, check if they are a bullet or mine, and kill them if they are

--- scripts/stage.gd
+++ scripts/stage.gd
@@ -14,20 +14,33 @@ func _process(_delta: float) -> void:
 	#var bullets = get_tree().get_nodes_in_group("Bullets")
 	#var mines = get_tree().get_nodes_in_group("Mines")
 	
+	#just get everyhing that exists right now
+	var nodes = get_tree().root.get_children()
+	
+	
 	if not $nav_map.is_baking():
 		$nav_map.bake_navigation_polygon()
 	
 	if enemies.is_empty() and player.is_empty():
+		for node in nodes:
+			if node.is_in_group("Bullets") or node.is_in_group("Mines"):
+				node.free()
 		#bullets.clear()
 		#mines.clear()
 		get_tree().change_scene_to_file("res://scenes/roundloss.tscn")
 	elif enemies.is_empty():	# If no enemies left
+		for node in nodes:
+			if node.is_in_group("Bullets") or node.is_in_group("Mines"):
+				node.free()
 		#for nodes in bullets:
 			#remove_child(nodes)
 		#for nodes in mines:
 			#remove_child(nodes)
 		get_tree().change_scene_to_file("res://scenes/roundwin.tscn")
 	elif player.is_empty():
+		for node in nodes:
+			if node.is_in_group("Bullets") or node.is_in_group("Mines"):
+				node.free()
 		#bullets.clear()
 		#mines.clear()
 		get_tree().change_scene_to_file("res://scenes/roundloss.tscn")
